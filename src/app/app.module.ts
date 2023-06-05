79f3be846f133a6e96ca58117f63a2277b670a87
Jordan Bannister <40512499+Jordanb1997@users.noreply.github.com>
2023-06-05 10:09:26 +1000

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactComponent } from './components/contact/contact.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkComponent } from './components/work/work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent
+    MainViewComponent,
+    ContactComponent,
+    SkillsComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
